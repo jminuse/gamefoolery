fea1fd7c5486adbd0f2b9a91d6731eb302ad1cb8
James Stevenson <jminuse@gmail.com>
2013-11-29 02:21:01 +0000

    Added platforms and collisions

--- js/script.js
+++ js/script.js
@@ -30,10 +30,38 @@ document.body.appendChild(me);
 me.style.position = 'absolute';
 me.style.color = 'red';
 
-me.x = window.innerWidth-100;
+me.x = 100;
 me.y = 200.0;
 me.dx = 0.0;
 me.dy = 0.0;
+
+EMPTY = 0; WALL = 1;
+map = [];
+S_shift = 3;
+S = 2<<(S_shift-1); //map discretization
+for(var x=0; x<window.innerWidth>>S_shift; x++) {
+  map[x] = [];
+  for(var y=0; y<window.innerHeight>>S_shift; y++) {
+    map[x][y] = EMPTY;
+  }
+}
+
+for(var i=0; i<3; i++) {
+  var mx = 10+i*15; var my = 3;
+  var block = document.createElement('div');
+  block.style.position = 'absolute';
+  block.style.left = (mx*S)+'px';
+  block.style.bottom = (my*S)+'px';
+  block.innerHTML = 'hello,world!';
+  document.body.appendChild(block);
+  var mw = block.offsetWidth>>S_shift;
+  var mh = block.offsetHeight>>S_shift;
+  for(var x=mx; x<mx+mw; x++) {
+    for(var y=my; y<my+mh; y++) {
+      map[x][y] = WALL;
+    }
+  }
+}
 
 UP = 38; DOWN = 40; LEFT = 37; RIGHT = 39;
 SPACE = 32;
@@ -47,10 +75,9 @@ window.onkeyup = function(k) {
 window.onkeydown = function(k) {
   keys_pressed[k.which] = true;
   //console.log(k.which);
-  if(k.which==SPACE) {
-    console.log(me.x, me.y, me.dx, me.dy);
-  }
 }
+
+var ground_bounce = -0.2;
 
 function render(time){
   if(!window.old_time) {
@@ -62,53 +89,95 @@ function render(time){
   }
   old_time = time;
   
+  var standing = me.y<0.1 || (map[me.x>>S_shift][(me.y-0.1)>>S_shift]!=EMPTY);
+  var near_standing = me.y<5.0 || (map[me.x>>S_shift][(me.y-5.0)>>S_shift]!=EMPTY);
+  
   if(keys_pressed[UP]) {
-    if(me.y<10.0 && Math.abs(me.dy)<0.1) {
-      me.dy += me.dy>0.0 ? 0.1 : -0.1;
+    if(near_standing) {
+      me.dy = 0.3;
+    }
+    if(!near_standing && me.dx<0.2) { //fall slower
+      me.dy += 0.0002*dt;
     }
   }
   if(keys_pressed[DOWN]) {
-    if(me.y<10.0 && Math.abs(me.dy)>0.05) {
-      me.dy *= 0.5;
+    if(near_standing) {
+      me.dx *= 0.8; me.dy *= 0.8;
+      if(Math.abs(me.dx)<0.02) me.dx = 0.0;
       if(Math.abs(me.dy)<0.02) me.dy = 0.0;
+    }
+    if(!near_standing && me.dx>-0.2) { //fall faster
+      me.dy -= 0.0002*dt;
     }
   }
   if(keys_pressed[LEFT]) {
-    if(me.y<10.0 && me.dx>-0.2) me.dx += -0.03;
+    if(near_standing && me.dx>-0.2) me.dx -= 0.003*dt;
+    if(!near_standing && me.dx>-0.2) { //directional fall
+      me.dx -= 0.0002*dt;
+    }
   }
   if(keys_pressed[RIGHT]) {
-    if(me.y<10.0 && me.dx<0.2) me.dx += 0.03;
+    if(near_standing && me.dx<0.2) me.dx += 0.003*dt;
+    if(!near_standing && me.dx<0.2) { //directional fall
+      me.dx += 0.0002*dt;
+    }
+  }
+  if(keys_pressed[SPACE]) {
+    console.log(me.x,me.y);
   }
   
-  if(me.y>0.0) {
-    me.dy -= 0.0005*dt;
+  if(!standing) { //gravity
+    me.dy -= 0.001*dt;
   }
-  if(me.y<10.0) {
-    if(me.dx<0.005 && me.dx>-0.005) { //slow to zero in x-direction
+  if(near_standing) {
+    if(me.dx<0.005 && me.dx>-0.005) { //zero out x-velocity
       me.dx = 0.0;
     }
     else { //apply friction
-      me.dx -= (me.dx>0 ? 0.0005 : -0.0005)*dt;
+      me.dx -= (me.dx>0.0 ? 0.001 : -0.001)*dt;
     }
   }
-  me.x += me.dx*dt; //integrate
-  me.y += me.dy*dt;
-  if(me.y<0.0) { //bounce off ground
+  
+  var new_x = me.x + me.dx*dt;
+  var new_y = me.y + me.dy*dt;
+  
+  if(new_y<0.0) { //bounce off ground
     me.y = 0.0;
-    if(me.dy<0.0) me.dy *= -0.4;
+    if(me.dy<0.0) me.dy *= ground_bounce;
   }
-  if(me.x<0.0) { //bounce off left wall
+  if(new_y>window.innerHeight-me.offsetHeight) { //bounce off ceiling
+    me.y = window.innerHeight-me.offsetHeight;
+    if(me.dy>0.0) me.dy *= ground_bounce;
+  }
+  if(new_x<0.0) { //bounce off left wall
     me.x = 0.0;
     if(me.dx<0.0) me.dx *= -0.8;
   }
-  if(me.x>window.innerWidth-me.offsetWidth) { //bounce off right wall
+  if(new_x>window.innerWidth-me.offsetWidth) { //bounce off right wall
     me.x = window.innerWidth-me.offsetWidth;
     if(me.dx>0.0) me.dx *= -0.8;
   }
-  if(me.y<2.0 && me.dy<0.005 && me.dy>-0.005) { //stop bounce in y-direction
-    me.y = 0.0
-    me.dy = 0.0;
+  
+  var new_x = me.x + me.dx*dt;
+  var new_y = me.y + me.dy*dt;
+  
+  var x = me.x; var y = me.y;
+  while((x-new_x)*me.dx<0.0 || (y-new_y)*me.dy<0.0) {
+    if(map[x>>S_shift][y>>S_shift]!=EMPTY) {
+      new_x = x==me.x ? x : x-me.dx*0.1;
+      new_y = y==me.y ? y : y-me.dy*0.1;
+      if(map[(x-me.dx*0.1)>>S_shift][y>>S_shift]==EMPTY) me.dx *= ground_bounce;
+      if(map[x>>S_shift][(y-me.dy*0.1)>>S_shift]==EMPTY) me.dy *= ground_bounce;
+      //console.log(':',new_x,new_y);
+      break;
+    }
+    x+=me.dx*0.1;
+    y+=me.dy*0.1;
   }
+  
+  me.x = new_x;
+  me.y = new_y;
+  
   me.style.left = (me.x+0.5|0)+'px'; //update screen image
   me.style.bottom = (me.y+0.5|0)+'px';
   requestAnimationFrame(render);
